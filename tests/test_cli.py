1c99ee6e99ba4db005aeb83bdfa9ccd55d591ec8
Tobias Buck <buck@mpia.de>
2024-03-28 20:53:26 +0100

    changed path to test sim

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,5 +5,5 @@ from click.testing import CliRunner
 
 def test_gsf_cli():
     runner = CliRunner()
-    result = runner.invoke(main, ['tests/sim1/sim1.halo_1.star.dat', 'tests/sim1/sim1.halo_1.gas.dat', 'tests/sim1/sim1.halo_1.dark.dat'])
+    result = runner.invoke(main, ['./sim1/sim1.halo_1.star.dat', './sim1/sim1.halo_1.gas.dat', './sim1/sim1.halo_1.dark.dat'])
     assert result.exit_code == 0
