78f0f39f7276da9f8de2046bff194c4653773f03
Tobias Buck <buck@mpia.de>
2024-03-28 19:04:16 +0100

    remove duplicate arguemtn from main.

--- gsf/__main__.py
+++ gsf/__main__.py
@@ -12,29 +12,27 @@ import click
 @click.argument('file_star', type=click.Path(exists=True, readable=True), nargs=1)
 @click.argument('file_gas', type=click.Path(exists=True, readable=True), nargs=1)
 @click.argument('file_dark', type=click.Path(exists=True, readable=True), nargs=1)
-@click.option('--out_dir', default=None, help='Path to the directory where all data should be saved. If None, gsf will create a new directory output/ in the running directory.')
 @click.option('--varlist', default='jzjc,jpjc,e', help='Comma separated names of the desired features on which to run the clustering.')
+@click.option('--doloop', is_flag=False, help='Run gsf in a loop to get the plot log Likelihood vs nk, and log Likelihood vs n_param.')
+@click.option('--out_dir', default=None, help='Path to the directory where all data should be saved. If None, gsf will create a new directory output/ in the running directory.')
 @click.option('--number_of_clusters', default=2, help='Number of multi-dimensional Gaussians or galaxy components.')
-@click.option('--doloop', is_flag=True, help='Run gsf in a loop to get the plot log Likelihood vs nk, and log Likelihood vs n_param.')
-@click.option('--plot', is_flag=True, help='Plot the 1st, 2nd and 3rd order moment maps for all components and the full galaxy in one figure.')
-@click.option('--n_init', default=1, help='The Expectation-Maximization algorithm of sklearn runs for up to 100 times or until the convergence threshold is met (1.0e-03), using n_init k-means initializations for the clusters.')
-@click.option('--band', is_flag=True, help='If the band is True, and there is a luminosity feature in the tmp_file, the moments maps figure will be done weighting with particle luminosity instead of particle mass.')
 @click.option('--eps', default=0.1, help='Gravitatonal softening in kpc.')
 @click.option('--radius_align', default=None, help='The 3D radius of the farthest particle to consider for orienting the galaxy.')
 @click.option('--trig_scaling', default=None, help='Setting any of the trig_scaling elements to True will result in the corresponding input feature being scaled as arctan(feature/std(feature)).')
 @click.option('--covariance_type', default='full', help='By default each clustering component is assumed to have its own general covariance matrix (full).')
 @click.option('--whiten_data', default=True, help='Center the clustering feature space to the mean and scaled to unit variance, feature by feature.')
+@click.option('--n_init', default=1, help='The Expectation-Maximization algorithm of sklearn runs for up to 100 times or until the convergence threshold is met (1.0e-03), using n_init k-means initializations for the clusters.')
+@click.option('--plot', is_flag=True, help='Plot the 1st, 2nd and 3rd order moment maps for all components and the full galaxy in one figure.')
+@click.option('--band', is_flag=True, help='If the band is True, and there is a luminosity feature in the tmp_file, the moments maps figure will be done weighting with particle luminosity instead of particle mass.')
 @click.option('--M2L', default=None, help='If the mass-to-light M2L is given, the weighting for the moemnts maps figure will be done with the luminosities computed from masses.')
 @click.option('--inclination', default=90., help='Angle in degrees setting the image perspective.')
 @click.option('--fov', default=None, help='Field-of-view value in kpc.')
 @click.option('--verbose', default=True, help='Gives some useful information that can e.g. speed up the run.')
 #@click.option('--filters', default=None, help='A dictionary with rules to select a subsample of the stellar particles for clustering using only LowPass, HighPass and BandPass on available or derivable features.')
 
-def main(file_star, file_gas, file_dark, out_dir=None, varlist='jzjc,jpjc,e', number_of_clusters=2,
-         doloop=True, plot=True, n_init=1, verbose=True, band=False,
-         eps=0.1, radius_align=None, trig_scaling=None,
-         covariance_type='full', whiten_data=True, 
-         M2L=False, inclination=90., fov=None, verbose=True):
+def main(file_star, file_gas, file_dark, varlist='jzjc,jpjc,e', doloop=False, out_dir=None, number_of_clusters=2,
+         eps=0.1, radius_align=None, trig_scaling=None, covariance_type='full', whiten_data=True, n_init=1, plot=True,
+         band=False, M2L=False, inclination=90., fov=None, verbose=True):
     """
     This is the main function of GalacticStructureFinder (GSF)
 
